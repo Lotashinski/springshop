a396c8a003f8a770e6b054ccb052b45a9673aacf
Alexander Lotashinsky <lat443152@gmail.com>
2025-05-16 12:32:55 +0300

    init

---
+++ src/main/java/com/github/lotashinski/App.java
@@ -0,0 +1,10 @@
+package com.github.lotashinski;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
