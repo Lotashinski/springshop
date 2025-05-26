f29d87409fc3f5bfe52dcd8eb777d73882591ffd
Alexander Lotashinsky <lat443152@gmail.com>
2025-05-26 18:40:36 +0300

    Create configured libs

--- ui/src/main/java/com/github/lotashinski/client/CategoriesClient.java
+++ ui/src/main/java/com/github/lotashinski/client/CategoriesClient.java
@@ -2,14 +2,13 @@ package com.github.lotashinski.client;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 import com.github.lotashinski.dto.CategoryCollectionItemDto;
 
+import feign.RequestLine;
 
-public interface CategoryClient {
+public interface CategoriesClient {
 
-	@GetMapping()
+	@RequestLine("GET /categories")
 	List<? extends CategoryCollectionItemDto> findByCriteria();
-	
+
 }
--- ui/src/main/java/com/github/lotashinski/service/impl/CategoryServiceImpl.java
+++ ui/src/main/java/com/github/lotashinski/service/impl/CategoryServiceImpl.java
@@ -2,15 +2,25 @@ package com.github.lotashinski.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.github.lotashinski.client.CategoriesClient;
 import com.github.lotashinski.dto.CategoryCollectionItemDto;
 import com.github.lotashinski.service.CategoriesService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoriesService {
 
+	private final CategoriesClient categories;
+	
 	@Override
 	public List<? extends CategoryCollectionItemDto> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return categories.findByCriteria();
 	}
 	
 }
