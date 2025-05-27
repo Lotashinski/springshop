466a0ace095e1ffc02a3aa25cde8189bf1818cc9
Alexander Lotashinsky <lat443152@gmail.com>
2025-05-27 15:30:35 +0300

    Create template sceleton

--- ui/src/main/java/com/github/lotashinski/ui/config/WebConfig.java
+++ ui/src/main/java/com/github/lotashinski/ui/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
@@ -16,6 +17,12 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 @ComponentScan("com.github.lotashinski")
 public class WebConfig implements WebMvcConfigurer {
 	 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+	
 	@Bean
 	public ClassLoaderTemplateResolver templateResolver() {
 		ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
