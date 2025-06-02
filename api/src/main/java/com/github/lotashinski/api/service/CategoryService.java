b94222a427c5bf9bf77ffc19f0386ad53409a654
Alexander Lotashinsky <lat443152@gmail.com>
2025-06-02 22:05:45 +0300

    Create api doc

--- api/src/main/java/com/github/lotashinski/api/service/CategoryService.java
+++ api/src/main/java/com/github/lotashinski/api/service/CategoryService.java
@@ -7,6 +7,7 @@ import com.github.lotashinski.api.dto.CategoryCriteriaDto;
 import com.github.lotashinski.api.dto.CategoryDataDto;
 import com.github.lotashinski.api.dto.CategoryDto;
 import com.github.lotashinski.api.entity.Category;
+import com.github.lotashinski.api.exceptions.ResourceNotFoundException;
 
 /**
  * CRUD service for working with {@code Category}
@@ -19,17 +20,46 @@ import com.github.lotashinski.api.entity.Category;
 public interface CategoryService {
 
 	/**
+	 * Get categories that satisfy the criteria
+	 * 
 	 * @param criteria
 	 * @return
 	 */
 	List<? extends CategoryCollectionItemDto> findByCriteria(CategoryCriteriaDto criteria);
 	
+	/**
+	 * Create new category
+	 * 
+	 * @param dto
+	 * @return
+	 */
 	CategoryDto create(CategoryDataDto dto);
 	
+	/**
+	 * Load category via identifier
+	 * 
+	 * @throws ResourceNotFoundException resource not found
+	 * @param id
+	 * @return
+	 */
 	CategoryDto read(Long id);
 	
+	/**
+	 * Update category via identifier
+	 * 
+	 * @throws ResourceNotFoundException resource not found
+	 * @param id
+	 * @param data
+	 * @return
+	 */
 	CategoryDto update(Long id, CategoryDataDto data);
 	
+	/**
+	 * Delete category via id
+	 * 
+	 * @throws ResourceNotFoundException resource not found
+	 * @param id
+	 */
 	void delete(Long id);
 	
 }
